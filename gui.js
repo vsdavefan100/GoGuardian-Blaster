2bc866567178daca879010f614d981bab9734c12
rice-a-rona <115025704+rice-a-rona@users.noreply.github.com>
2022-10-04 08:35:51 -0700

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/3kh0/ext-remover/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/rice-a-rona/ext-remover/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
